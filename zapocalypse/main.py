dff2f5ed351dfc700ef021c2fb7f7b8c2f5703fe
gagaruk <red8gams@gmail.com>
2026-04-09 23:10:24 +0300

    setting up the environment

--- zapocalypse/main.py
+++ zapocalypse/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import h3
 
 from supabase import Client
 
@@ -11,7 +12,7 @@ class HordeObservation(BaseModel):
     lat: int
     lng: int
 
-@app.post("/horde/update"):
+@app.post("/horde/update")
 async def update_horde(observation: HordeObservation):
     h3_cell = h3.latlng_to_h3(observation.lat, observation.lng, 9)
 
