1b13a96050f3007fa8f207cadabf08eef4e169fa
gagaruk <red8gams@gmail.com>
2026-04-09 09:14:05 +0300

    init

---
+++ zapocalypse/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from supabase import Client
+
+app = FastAPI()
+
+class HordeObservation(BaseModel):
+    horde_id: int
+    est_count: int
+    lat: int
+    lng: int
+
+@app.post("/horde/update"):
+async def update_horde(observation: HordeObservation):
+    h3_cell = h3.latlng_to_h3(observation.lat, observation.lng, 9)
+
+    return {"status": "success", "h3_index": h3_cell}
+
+
